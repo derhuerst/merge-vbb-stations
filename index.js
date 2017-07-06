7da9e5d7fb44046a18b4078007e9635c57d7f400
Jannis R <mail@jannisr.de>
2017-07-06 17:41:42 +0200

    improve algorithm
    - shorten station names
    - ignore identical stations
    - ignore subsequent stations by comparing lines

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict'
 
+const linesAt = require('vbb-lines-at')
 const distance = require('gps-distance')
+const shorten = require('vbb-short-station-name')
 const leven = require('leven')
 
-const JOIN = 'join'
+const MERGE = 'merge'
+const MERGE_AS_STOP = 'mergeAsStop'
 
 const _ = (n) => +n.toFixed(3)
 
 const analyse = (s1, s2) => {
+	if (s1.id === s2.id) return null // they seem to be the same
+
 	// don't merge stops with stations
 	if (s1.type !== s2.type) return null
 
@@ -18,28 +23,35 @@ const analyse = (s1, s2) => {
 	// cheap cutoff: stations more than 250m apart can be ignored
 	if (km >= .25) return null
 
-	const nameDifference = leven(s1.name, s2.name)
+	const n1 = shorten(s1.name)
+	const n2 = shorten(s2.name)
+	const sS = n1.length < n2.length ? s1 : s2 // station with shorter name
+	const sL = n1.length < n2.length ? s2 : s1 // station with longer name
 
-	if (nameDifference === 0) return JOIN
+	const nameDifference = leven(n1, n2)
+	if (nameDifference === 0) return [MERGE]
 	if (nameDifference === 1 && km <= .15) {
-		// todo: decide which direction to merge
-		return JOIN
+		// todo: find a better heuristic
+		// always merge into the station with the shorter name
+		return [MERGE_AS_STOP, sL, sS]
 	}
 
-	const sN = s1.name.length < s2.name.length ? s1.name : s2.name // short name
-	const lN = s1.name.length < s2.name.length ? s2.name : s1.name // long name
-	const nameSimilarity = sN.length / lN.length
+	const linesAt1 = linesAt[s1.id].map((l) => l.id)
+	const linesAt2 = linesAt[s2.id].map((l) => l.id)
+	const commonLines = linesAt1.some((l) => linesAt2.includes(l))
+	if (commonLines) return null // more likely to be subsequent stations
+
+	const sN = n1.length < n2.length ? n1 : n2 // short name
+	const lN = n1.length < n2.length ? n2 : n1 // long name
 	const haveSameStem = lN.slice(0, sN.length) === sN
 
 	// find "U FooBar" & "U FooBar Baz", ignore "U Foostr." & "U Foostr./Barstr."
 	if (haveSameStem && lN[sN.length] !== '/') {
-		const score = km * 1000  / nameSimilarity // weigh distance & similarity
-		console.error(sN, _(km) * 1000, _(nameSimilarity), _(score), lN)
-		// todo
-		return null
+		// always merge into the station with the shorter name
+		return [MERGE_AS_STOP, sL, sS]
 	}
 
 	return null
 }
 
-module.exports = Object.assign(analyse, {JOIN})
+module.exports = Object.assign(analyse, {MERGE, MERGE_AS_STOP})
